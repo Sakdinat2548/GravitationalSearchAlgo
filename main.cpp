999da1dda44dc63225ebdb775c7829bd9eeecb38
Sakdinat2548 <sakdinat.kuy@gmail.com>
2026-08-03 08:52:08 +0700

    shorter configs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
+#include <cmath>
 #include <iostream>
 #include <vector>
-#include <cmath>
+
 #include "Gsa.hpp"
 
 double sphere(const std::vector<double>& x) {
@@ -12,7 +13,8 @@ double sphere(const std::vector<double>& x) {
 double rosenbrock(const std::vector<double>& x) {
     double sum = 0.0;
     for (size_t i = 0; i < x.size() - 1; ++i) {
-        sum += 100.0 * std::pow(x[i + 1] - std::pow(x[i], 2), 2) + std::pow(x[i] - 1, 2);
+        sum += 100.0 * std::pow(x[i + 1] - std::pow(x[i], 2), 2) +
+               std::pow(x[i] - 1, 2);
     }
     return sum;
 }
@@ -22,24 +24,23 @@ int main() {
     // GravitationalSearchAlgorithm gsa1(3, -5.0, 5.0, sphere);
     // auto [val1, pos1] = gsa1.optimize();
 
-
-    // // --- WAY 2: Inline Vector List Bounds (3 dimensions with unique bounds) ---
-    // GravitationalSearchAlgorithm gsa2({-10.0, 0.0, -1.0}, {10.0, 50.0, 1.0}, sphere);
-    // auto [val2, pos2] = gsa2.optimize();
+    // // --- WAY 2: Inline Vector List Bounds (3 dimensions with unique bounds)
+    // --- GravitationalSearchAlgorithm gsa2({-10.0, 0.0, -1.0},
+    // {10.0, 50.0, 1.0}, sphere); auto [val2, pos2] = gsa2.optimize();
 
     // std::cout << "Inline Scalar Bounds Result: " << val1 << "\n";
     // std::cout << "Inline Vector Bounds Result: " << val2 << "\n";
 
-    GsaConfig custom_config;
-    custom_config.n_agents = 50;
-    custom_config.max_iter = 5000;
-    custom_config.g0 = 1.0;
-
-    GravitationalSearchAlgorithm gsa3(3, -2.048, 2.048, rosenbrock, custom_config);
+    GravitationalSearchAlgorithm gsa3(3, -2.048, 2.048, rosenbrock,
+                                      {.n_agents = 50,
+                                       .max_iter = 5000,
+                                       .g0 = 10.0,
+                                       .alpha = 10.0,
+                                       .minimize = true});
     auto [val3, pos3] = gsa3.optimize();
 
     std::cout << "Rosenbrock Result: " << val3 << "\n";
-    for(auto &p : pos3) std::cout << p << " ";
+    for (auto& p : pos3) std::cout << p << " ";
     std::cout << "\n";
 
     return 0;
